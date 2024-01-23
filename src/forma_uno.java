a51601093d06dc2777dad0a629bf768680730fcb
Jose Mateo Bernal <jose.bernal@epn.edu.ec>
2024-01-23 08:22:15 -0500

    interfaz 3

--- src/forma_uno.java
+++ src/forma_uno.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class forma_uno {
     JPanel panel_principal;
@@ -7,4 +10,26 @@ public class forma_uno {
     private JPasswordField ingreso_password;
     private JLabel texto_usuario;
     private JButton INICIARSESIONButton;
+
+
+    public forma_uno() {
+        INICIARSESIONButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String  usuario= "admin" ;
+                String clave = "password";
+                String usuario_ingresado=ingreso_correo.getText();
+                String password_ingresado=ingreso_password.getText();
+
+                if (usuario.equals(usuario_ingresado)&& password_ingresado.equals(password_ingresado)){
+                    System.out.println("Hola");
+
+                }else{
+                    System.out.println("Error");
+                }
+
+            }
+        });
+    }
 }

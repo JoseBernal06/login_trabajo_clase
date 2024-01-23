0b0cb78cb724fd0e8458f15e3b7a0cd46bd91876
Jose Mateo Bernal <jose.bernal@epn.edu.ec>
2024-01-23 08:22:15 -0500

    interfaz

--- src/forma_uno.java
+++ src/forma_uno.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class forma_uno {
+import javax.swing.*;
+
+public class forma_uno {
+    JPanel panel_principal;
+    private JLabel titulo;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JLabel texto_usuario;
+    private JButton INICIARSESIONButton;
 }
